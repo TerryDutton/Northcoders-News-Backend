17cef814e2656d24c4295a179ca9250c36b619e6
TerryDutton <t_dutton85@hotmail.com>
2018-05-11 15:57:30 +0100

    Repaired frontpage rendering

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const app = require('express')(); 
+const express = require('express');
+const app = express(); 
 const bodyParser = require('body-parser'); 
 const {DB_URI} = process.env.NODE_ENV === 'production' ? process.env : require('./config/index.js');
 const mongoose = require('mongoose'); 
@@ -9,13 +10,19 @@ mongoose.connect(DB_URI)
 
 app.use(bodyParser.json());
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs'); 
+
 app.use('/api', apiRouter);
 
 app.use('*', (req, res, next) => next({status:404, message: 'Page not found'}));
 
 app.use((err, req, res, next) => {
-  return err.status !== 500 ? res.status(err.status).send({message: err.message})
-  : res.status(500).send({message: 'Internal Server Error'}); 
+  if (err.status && err.status !== 500) return res.status(err.status).send({message: err.message});
+  else {
+    console.log(err);
+    return res.status(500).send({message: 'Internal Server Error'});
+  } 
 });
 
 module.exports = app;
